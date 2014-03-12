d925a0f35375f08505be6e0b33c724db067b5873
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-03-12 15:43:45 +0800

    Prep for password encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
+require 'bcrypt'
+
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
 
   field :email, type: String
+  field :saffron, type: String
+  field :fugu, type: String
 end
