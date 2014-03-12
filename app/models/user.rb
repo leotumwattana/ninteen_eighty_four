64b6f6d090ce736ed368320a67c59aae2498a693
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-03-12 17:09:26 +0800

    Adds basic password encryption before saving user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,19 @@ class User
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  attr_accessor :password, :password_confirmation
+
   field :email, type: String
-  field :saffron, type: String
+  field :wasabi, type: String
   field :fugu, type: String
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    if password.present?
+      self.wasabi = BCrypt::Engine.generate_salt
+      self.fugu = BCrypt::Engine.hash_secret(password, self.wasabi)
+    end
+  end
+
 end
