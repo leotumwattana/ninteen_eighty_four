e38265170b3352ffe7dcedb6e56abcf9e8afb5b8
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-03-13 10:37:36 +0800

    Adds SessionController and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 NinteenEightyFour::Application.routes.draw do
 
   root 'site#index'
-  get  '/privacy' => 'site#privacy'
-  get  '/terms'   => 'site#terms'
+
+  get     'login'  => 'session#new'
+  post    'login'  => 'session#create'
+  delete  'logout' => 'session#destroy'
+  get     'logout' => 'session#destroy'
+
+  get '/privacy' => 'site#privacy'
+  get '/terms'   => 'site#terms'
 end
