3231fa58a6cbb9b8f445b5b4888c6c105fef19c6
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-03-14 11:05:42 +0800

    Adds is_authenticate?

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -8,13 +8,16 @@ class SessionController < ApplicationController
     @user = User.authenticate(params[:user][:email], params[:user][:password])
 
     if @user
-      render text: "#{@user.email} is in the house yo!"
+      session[:user_id] = @user.id
+      redirect_to root_url
     else
       render text: "Who the funk are you"
     end
+
   end
 
   def destroy
+    session[:user_id] = nil
     render text: "Log the user out."
   end
 
