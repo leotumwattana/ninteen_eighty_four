e1123952c0d9becf42c7dda0f1eeef796d9677f9
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-03-17 10:32:00 +0800

    Adds logout link

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ NinteenEightyFour::Application.routes.draw do
   get     'login'  => 'session#new'
   post    'login'  => 'session#create'
   delete  'logout' => 'session#destroy'
-  get     'logout' => 'session#destroy' # TODO: Remove before deployment
 
   get '/privacy' => 'site#privacy'
   get '/terms'   => 'site#terms'
