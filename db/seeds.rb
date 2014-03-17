e92718aaecd962b23ef53c2ba7977901d6f30abb
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-03-17 10:12:48 +0800

    Updates seed.rb with more sample accounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 User.destroy_all
+
 User.create([
-  { email: "annie@gmail.com", password: "iloveseaweed", password_confirmation: "iloveseaweed" },
-  { email: "kulio@gmail.com", password: "leonation", password_confirmation: "leonation" }
+  {
+    email: "annie@gmail.com",
+    password: "iloveseaweed",
+    password_confirmation: "iloveseaweed"
+  },
+  {
+    email: "kulio@gmail.com",
+    password: "123",
+    password_confirmation: "123"
+  },
+  {
+    email: "mark@gmail.com",
+    password: "sake",
+    password_confirmation: "sake"
+  },
+  {
+    email: "marco@gmail.com",
+    password: "wegotdeathstar",
+    password_confirmation: "wegotdeathstar"
+  },
+  {
+    email: "christian@gmail.com",
+    password: "integrity",
+    password_confirmation: "integrity"
+  },
+  {
+    email: "anil@gmail.com",
+    password: "nil",
+    password_confirmation: "nil"
+  },
+  {
+    email: "simon@gmail.com",
+    password: "says",
+    password_confirmation: "says"
+  },
+  {
+    email: "charles@gmail.com",
+    password: "ruleroftheworld",
+    password_confirmation: "ruleroftheworld"
+  },
+  {
+    email: "harry@gmail.com",
+    password: "speeddemon",
+    password_confirmation: "speeddemon"
+  },
 ])
