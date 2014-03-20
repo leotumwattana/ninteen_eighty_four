9f03cd3e2527ce96fb4d4d99b8c59717ca28038c
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-03-20 23:17:57 +0800

    Updates UserController

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,24 +1,56 @@
 class UserController < ApplicationController
 
+  CREATE_FAILED = "We were unable to create your account."
+  UPDATE_FAILED = "Oppss...how embarrassing. We're not able to update your account information at this time."
+
   def index
+    @users = User.all.entries
   end
 
   def show
+    @user = User.find_by(email: params[:email])
   end
 
   def new
   end
 
   def create
+    if @user = User.create(user_params)
+      # success
+      log_user_in(@user)
+      redirect_to root_url
+    else
+      # fail
+      flash.now[:alert] = CREATE_FAILED
+      render :new
+    end
   end
 
   def edit
+    @user = User.find_by(email: params[:email])
   end
 
   def update
+    @user = User.find_by(email: params[:email])
+    if @user.update_attributes(user_params)
+      # success
+      log_user_in(@user)
+      redirect_to root_url
+    else
+      # fail
+      flash.now[:alert] = UPDATE_FAILED
+    end
   end
 
   def destroy
+    user = User.find_by(email: params[:email])
+    user.destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
 end
