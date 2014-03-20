6a210e5faba05c1125d93ea2fe865f397fa71ea6
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-03-20 09:21:42 +0800

    Adds BMail model

---
+++ app/models/b_mail.rb
@@ -0,0 +1,26 @@
+class BMail
+
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :title, type: String
+  field :to, type: Array
+  field :content, type: String
+
+  validates :title, presence: true
+  validates :to, presence: true
+  validates :content, presence: true
+
+  alias _to= to=
+
+  def to=(emails)
+    self._to = emails.split(',').map { |e| e.strip.chomp }
+  end
+
+  private
+
+  def split_emails(emails)
+    emails.split(',').map { |e| e.strip }
+  end
+
+end
