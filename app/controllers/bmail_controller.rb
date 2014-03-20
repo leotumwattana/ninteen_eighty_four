d30772ef86f7bbf498f24ea4c92025a23d3714b6
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-03-20 13:56:13 +0800

    Updates bmail_controller

--- app/controllers/bmail_controller.rb
+++ app/controllers/bmail_controller.rb
@@ -1,27 +1,39 @@
 class BmailController < ApplicationController
+  layout "main"
 
   def index
     @bmails = Bmail.all.entries
   end
 
   def show
-    id = params[:id]
-    @bmail = Bmail.find_by(id: id)
+    @bmail = Bmail.find_by(id: params[:id])
   end
 
   def new
   end
 
   def create
+    Bmail.create(bmail_params)
+    redirect_to bmail_index_path
   end
 
   def edit
+    @bmail = Bmail.find_by(id: params[:id])
   end
 
   def update
+    bmail = Bmail.find_by(id: params[:id])
+    bmail.update_attributes(bmail_params)
+    redirect_to bmail_index_path
   end
 
   def destroy
+  end
+
+  private
+
+  def bmail_params
+    params.require(:bmail).permit(:title, :to, :content)
   end
 
 end
