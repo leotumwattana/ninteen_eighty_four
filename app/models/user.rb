a401a4e42587d1e14d111ce4027af17bb5feb9ac
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-03-29 10:24:27 +0800

    Updates user model to pass user_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 require 'bcrypt'
 
 PASSWORD_RESET_EXPIRES = 4
+EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i
 
 class User
   include Mongoid::Document
@@ -19,7 +20,10 @@ class User
   before_save :set_random_password, :encrypt_password
 
   validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :email, format: { with: EMAIL_REGEX }
   validates :password, confirmation: true
+  validates :salt, presence: true
+  validates :fugu, presence: true
 
   def self.authenticate(email, password)
     user = User.find_by email: email
