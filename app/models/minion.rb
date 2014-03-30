21151df0e30070a06586c7c2b08c06f6dd27febc
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-03-30 18:24:41 +0800

    Change minion.rb to bail_alert_worker.rb

--- app/models/minion.rb
+++
@@ -1,8 +0,0 @@
-class Minion
-  include Sidekiq::Worker
-  sidekiq_options queue: "high"
-
-  def perform(scheduled_at)
-    puts "Minion #{scheduled_at}: performing task at: #{Time.now}"
-  end
-end
---
+++ app/workers/bail_alert_worker.rb
@@ -0,0 +1,9 @@
+class BailAlertWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: "high"
+
+  def perform(bmail_id)
+    bmail = Bmail.find(bmail_id)
+    Bmailer.notify_user(bmail).deliver
+  end
+end
