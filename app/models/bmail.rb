ced09c9c68202ed9b1f6faac63e066f7643f91d4
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-03-30 18:26:37 +0800

    Adds time_zone attr and before_save filter to compensate timezone when present

--- app/models/bmail.rb
+++ app/models/bmail.rb
@@ -3,12 +3,16 @@ class Bmail
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  attr_accessor :time_zone
+
   field :title, type: String
   field :to, type: Array
   field :content, type: String
   field :trigger_date, type: DateTime
 
   belongs_to :user, dependent: :nullify
+
+  before_save :update_time_zone
 
   scope :active, -> { where(:trigger_date.ne => nil, :trigger_date.gt => Time.now).asc(:trigger_date) }
   scope :pending, -> { where(:trigger_date => nil).order_by(:created_at.desc) }
@@ -34,6 +38,12 @@ class Bmail
   end
 
   private
+
+  def update_time_zone
+    if not (self.time_zone.blank? || self.time_zone.nil?)
+      self.trigger_date -= self.time_zone.to_i.hours
+    end
+  end
 
   def split_emails(emails)
     emails.split(',').map { |e| e.strip }
