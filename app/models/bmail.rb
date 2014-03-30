e5974f3bcb9f97ade05ebed55955950c840f9404
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-03-31 03:54:04 +0800

    Fixes bmail scope and adds schedule_delivery

--- app/models/bmail.rb
+++ app/models/bmail.rb
@@ -1,6 +1,8 @@
 class Bmail
 
   ACTIVE_MONGOID_JS_QUERY = "(this.trigger_date - (this.time_zone * 60 * 60 * 1000)) > new Date().getTime()"
+  SENT_MONGOID_JS_QUERY = "(this.trigger_date - (this.time_zone * 60 * 60 * 1000)) <= new Date().getTime()"
+  ADVANCED_ALERT_PERIOD = 1.hour
 
   include Mongoid::Document
   include Mongoid::Timestamps
@@ -14,11 +16,11 @@ class Bmail
 
   belongs_to :user, dependent: :nullify
 
-  after_save :schedule_bmail
+  after_save :schedule_advanced_alert, :schedule_delivery
 
   scope :active, -> { where(:trigger_date.ne => nil).where(ACTIVE_MONGOID_JS_QUERY).desc(:trigger_date) }
-  scope :pending, -> { where(:trigger_date => nil).order_by(:created_at.desc) }
-  scope :sent, -> { where(:trigger_date.lte => Time.now, :trigger_date.ne => nil).order_by(:trigger_date.asc) }
+  scope :pending, -> { where(:trigger_date => nil).desc(:created_at) }
+  scope :sent, -> { where(:trigger_date.ne => nil).where(SENT_MONGOID_JS_QUERY).desc(:trigger_date) }
 
   validates :title, presence: true
   validates :to, presence: true
@@ -41,9 +43,16 @@ class Bmail
 
   private
 
-  def schedule_bmail
+  def schedule_advanced_alert
     unless trigger_date.nil?
-      BailAlertWorker.perform_at(trigger_date - time_zone.hours, id.to_s)
+      BailAlertWorker.perform_at(trigger_date - time_zone.hours - ADVANCED_ALERT_PERIOD, id.to_s)
+    end
+  end
+
+  def schedule_delivery
+    unless trigger_date.nil?
+      schedule_id = BailMailWorker.perform_at(trigger_date - time_zone.hours, id.to_s)
+      puts "THIS IS THE SCHEDULE ID: " + schedule_id.to_s
     end
   end
 
