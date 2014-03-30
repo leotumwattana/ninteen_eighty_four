cc02e760389808aae05763f87068c28a313104ae
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-03-31 03:51:25 +0800

    Updates Bmailer with send_bmail and updates notify_user

--- app/mailers/bmailer.rb
+++ app/mailers/bmailer.rb
@@ -3,14 +3,18 @@ class Bmailer < ActionMailer::Base
   NOTIFY_PREFIX = "Your BMail: "
   NOTIFY_SUFFIX = " is about to be delivered."
 
+  default from: "BMail <support@collateral.com>"
+
   def notify_user(bmail)
     @bmail = bmail
     user = bmail.user
-    mail from: user.email, to: bmail.to_as_array, subject: NOTIFY_PREFIX + bmail.title + NOTIFY_SUFFIX
+    mail to: user.email, subject: NOTIFY_PREFIX + bmail.title + NOTIFY_SUFFIX
   end
 
   def send_bmail(bmail)
-
+    @bmail = bmail
+    user = bmail.user
+    mail from: user.email, to: bmail.to_as_array, subject: bmail.title
   end
 
 end
